15a5ca612fdb18a4fccfeb62386f90a422674cfc
iluhcm <sam.lxing@gmail.com>
2014-12-26 17:23:20 +0800

    Add the file parsers independently for the reference files and input them to the database.

---
+++ src/dataparser/DARNEDParser.java
@@ -0,0 +1,103 @@
+/*
+ * REFilter: RNA Editing Filter
+ *     Copyright (C) <2014>  <Xing Li>
+ *
+ *     REFilter is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     REFilter is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dataparser;
+
+import database.DatabaseManager;
+import database.TableCreator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.Indexer;
+import utils.Timer;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+
+/**
+ * P_value based on alt and ref
+ */
+public class DARNEDParser {
+    private static final Logger logger = LoggerFactory.getLogger(DARNEDParser.class);
+    private DatabaseManager databaseManager;
+
+    public DARNEDParser(DatabaseManager databaseManager) {
+        this.databaseManager = databaseManager;
+    }
+
+    private void createDARNEDTable(final String tableName) {
+        try {
+            if (!databaseManager.existTable(tableName)) {
+                //"(chrom varchar(15),coordinate int,strand varchar(5),inchr varchar(5), inrna varchar(5) ,index(chrom,coordinate))");
+                TableCreator.createReferenceTable(tableName, new String[]{"chrom", "coordinate", "strand", "inchr", "inrna"}, new String[]{
+                        "varchar(30)",
+                        "int", "varchar(5)", "varchar(5)", "varchar(5)"
+                }, Indexer.CHROM_COORDINATE);
+            }
+        } catch (SQLException e) {
+            logger.error("Error create DARNED table: " + tableName, e);
+        }
+    }
+
+    public void loadDarnedTable(String darnedPath) {
+        logger.info("Start loading DARNED file into database...\t" + Timer.getCurrentTime());
+        String darnedTable = DatabaseManager.DARNED_DATABASE_TABLE_NAME;
+        if (!databaseManager.hasEstablishTable(darnedTable)) {
+            createDARNEDTable(darnedTable);
+            try {
+                int count = 0;
+                databaseManager.setAutoCommit(false);
+                FileInputStream inputStream = new FileInputStream(darnedPath);
+                BufferedReader rin = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                // Skip the first row.
+                rin.readLine();
+                while ((line = rin.readLine()) != null) {
+                    String[] sections = line.trim().split("\\t");
+                    StringBuilder stringBuilder = new StringBuilder("insert into ");
+                    stringBuilder.append(darnedTable);
+                    stringBuilder.append("(chrom,coordinate,strand,inchr,inrna) values(");
+                    for (int i = 0; i < 5; i++) {
+                        if (i == 0) {
+                            stringBuilder.append("'chr").append(sections[i]).append("',");
+                        } else if (i == 1) {
+                            stringBuilder.append(sections[i]).append(",");
+                        } else {
+                            stringBuilder.append("'").append(sections[i]).append("',");
+                        }
+                    }
+                    stringBuilder.deleteCharAt(stringBuilder.length() - 1).append(")");
+
+                    databaseManager.executeSQL(stringBuilder.toString());
+                    if (++count % DatabaseManager.COMMIT_COUNTS_PER_ONCE == 0)
+                        databaseManager.commit();
+                }
+                databaseManager.commit();
+                databaseManager.setAutoCommit(true);
+            } catch (IOException e) {
+                logger.error("Error load file from " + darnedPath + " to file stream", e);
+            } catch (SQLException e) {
+                logger.error("Error execute sql clause in " + DARNEDParser.class.getName() + ":loadDarnedTable()", e);
+                return;
+            }
+        }
+        logger.info("End loading DARNED file into database...\t" + Timer.getCurrentTime());
+    }
+}
---
+++ src/dataparser/RepeatMaskerParser.java
@@ -0,0 +1,100 @@
+/*
+ * REFilter: RNA Editing Filter
+ *     Copyright (C) <2014>  <Xing Li>
+ *
+ *     REFilter is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     REFilter is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dataparser;
+
+import database.DatabaseManager;
+import database.TableCreator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.Indexer;
+import utils.Timer;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+
+/**
+ * we will filter out base in repeated area except for SINE/alu
+ */
+public class RepeatMaskerParser {
+    private static final Logger logger = LoggerFactory.getLogger(RepeatMaskerParser.class);
+    private DatabaseManager databaseManager;
+
+    public RepeatMaskerParser(DatabaseManager databaseManager) {
+        this.databaseManager = databaseManager;
+    }
+
+    private void createRepeatRegionsTable(final String tableName) {
+        try {
+            if (!databaseManager.existTable(tableName)) {
+                //chrom varchar(30),begin int,end int,type varchar(40),index(chrom,begin,end);
+                TableCreator.createReferenceTable(tableName, new String[]{"chrom", "begin", "end", "type"},
+                        new String[]{"varchar(30)", "int", "int", "varchar(40)"}, Indexer.CHROM_BEGIN_END);
+            }
+        } catch (SQLException e) {
+            logger.error("Error create repeat regions table", e);
+            e.printStackTrace();
+        }
+    }
+
+    public void loadRepeatTable(String repeatPath) {
+        logger.info("Start loading RepeatMasker file into database...\t" + Timer.getCurrentTime());
+        BufferedReader rin = null;
+        String repeatTable = DatabaseManager.REPEAT_MASKER_TABLE_NAME;
+        try {
+            if (!databaseManager.hasEstablishTable(repeatTable)) {
+                createRepeatRegionsTable(repeatTable);
+                databaseManager.setAutoCommit(false);
+                int count = 0;
+                FileInputStream inputStream = new FileInputStream(repeatPath);
+                rin = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                rin.readLine();
+                rin.readLine();
+                rin.readLine();
+                while ((line = rin.readLine()) != null) {
+                    String section[] = line.trim().split("\\s+");
+                    databaseManager.executeSQL("insert into " + repeatTable + "(chrom,begin,end,type) values('" +
+                            section[4] + "','" + section[5] + "','" + section[6] + "','" + section[10] + "')");
+                    if (++count % DatabaseManager.COMMIT_COUNTS_PER_ONCE == 0)
+                        databaseManager.commit();
+                }
+                databaseManager.commit();
+                databaseManager.setAutoCommit(true);
+            }
+
+        } catch (IOException e) {
+            logger.error("Error load file from " + repeatPath + " to file stream", e);
+        } catch (SQLException e) {
+            logger.error("Error execute sql clause in " + RepeatMaskerParser.class.getName() + ":loadRepeatTable()", e);
+            return;
+        } finally {
+            if (rin != null) {
+                try {
+                    rin.close();
+                } catch (IOException e) {
+                    logger.error("", e);
+                }
+            }
+        }
+        logger.info("End loading RepeatMasker file into database...\t" + Timer.getCurrentTime());
+    }
+}
